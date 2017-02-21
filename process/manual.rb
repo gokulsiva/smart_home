19dbe0ed12d327b05bab6b05f00467f0c232886a
gokulsiva <gsgokul305@gmail.com>
2017-02-21 23:52:04 +0530

    Tue Feb 21 23:52:04 IST 2017

--- process/manual.rb
+++ process/manual.rb
@@ -1,16 +1,13 @@
-require 'pi_piper'
-include PiPiper
-require '~/Sites/smart_home/config/environment'
-
+require File.expand_path('/home/pi/Sites/smart_home/config/environment', __FILE__)
 
 loop do
   controls = Control.all
   for control in controls
-    pin = PiPiper::Pin.new(:pin => control.pin, :direction => :out)
-    if control.automated
-      pin.on
-    else
-      pin.off
-    end
+   system "gpio -g mode #{control.pin} out"
+   if control.automated
+	system "gpio -g write #{control.pin} 1"
+   else
+	system "gpio -g write #{control.pin} 0"
+  end
   end
 end
