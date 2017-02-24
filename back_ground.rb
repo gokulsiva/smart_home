dc34bf3138239b580e2c83090c7fd7f0775dde5e
gokulsiva <gsgokul305@gmail.com>
2017-02-24 16:30:29 +0530

    Fri Feb 24 16:30:29 IST 2017

---
+++ back_ground.rb
@@ -0,0 +1,40 @@
+require File.expand_path('/home/pi/Sites/smart_home/config/environment', __FILE__)
+
+command = Thread.new do
+  system('ruby /home/pi/Sites/smart_home/automatic_controller.rb stop') # long-long programm
+end
+command.join
+
+puts "Automatic controller stopped."
+
+command = Thread.new do
+  system('ruby /home/pi/Sites/smart_home/manual_controller.rb stop') # long-long programm
+end
+command.join
+
+puts "Manual controller controller stopped."
+
+command = Thread.new do
+  system('ruby /home/pi/Sites/smart_home/resetpins.rb') # long-long programm
+end
+command.join
+
+puts "Pins resetted".
+
+switch = MainSwitch.first
+if switch
+  if switch.control
+    command = Thread.new do
+      system('ruby /home/pi/Sites/smart_home/automatic_controller.rb start') # long-long programm
+    end
+    command.join
+    puts "Automattic controller startted"
+  else
+    command = Thread.new do
+      system('ruby /home/pi/Sites/smart_home/manual_controller.rb start') # long-long programm
+    end
+    command.join
+    puts "Manual controller startted"
+  end
+end
+end
