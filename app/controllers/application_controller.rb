b0c95e6d2014ec118529f778fcfff0ac218f5da0
gokulsiva <gsgokul305@gmail.com>
2017-02-24 15:52:06 +0530

    Fri Feb 24 15:52:06 IST 2017

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+  before_action :initializer
+
+
+  private
+
+  def initializer
+    system "ruby /home/pi/Sites/smart_home/automatic_controller.rb stop"
+    system "ruby /home/pi/Sites/smart_home/manual_controller.rb stop"
+    system "ruby /home/pi/Sites/smart_home/resetpins.rb"
+    switch = MainSwitch.first
+    if switch
+      if switch.control
+        system "ruby /home/pi/Sites/smart_home/automatic_controller.rb start"
+      else
+        system "ruby /home/pi/Sites/smart_home/manual_controller.rb start"
+      end
+    end
+  end
 end
